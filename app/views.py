8a4fe4ec5c8e6e245c2a220a537136b29aeecb52
king strong <king strong>
2017-09-14 01:02:17 +0800

    1

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from app import app
+from flask import render_template,flash,redirect
+from .forms import LoginForm
+@app.route('/')
+@app.route("/index")
+def index():
+    user = {'nickname':'roy'}
+    posts = [
+        {
+            'author':{'nickname':'john'},
+            'body':'Brautiful day in shanghang'
+        },
+        {
+            'author':{'nickname':'roy'},
+            'body':'coooooooooooooool'
+        }
+    ]
+    return render_template("index.html",
+                           title = 'home',
+                           user = user,
+                           posts = posts)
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
